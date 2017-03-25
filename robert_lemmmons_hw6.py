ec944d16f97394229c0b05ea7c2a88a0a41e0151
Rob Lemmons <robertlemmons@mail.weber.edu>
2017-03-24 20:05:50 -0600

    init commit again

---
+++ robert_lemmmons_hw6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+"""
+Create a script that opens an Apache server error log file
+ and retrieves the top 25 errors
+"""
+
+
+
+# Main function
+def main():
+    """
+    Test Function
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
