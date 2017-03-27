cf5ed944b5f357d65bde10d6047b7523db4b9261
Rob Lemmons <robertlemmons@mail.weber.edu>
2017-03-26 20:06:15 -0600

    file pull done, needs sort

--- robert_lemmmons_hw6.py
+++ robert_lemmmons_hw6.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python3
 import sys
+import re
+from urllib.request import urlopen
+
 """
 Create a script that opens an Apache server error log file
  and retrieves the top 25 errors
 """
 
 
+def logFile(url):
+    """
+    use URLOPEN to retrieve log file
+    """
+    with urlopen(url) as log:
+        for line in log:
+            print(line)
+
+
+def sortstuff():
+    """
+    this should sort retrieved error file
+    to get top 25 most common errors
+    """
+    pass
+
 
 # Main function
 def main():
     """
-    Test Function
+    Something something from a URL
+    Usage:
+        python3 robert_lemmons_hw6.py <URL>
     """
-    pass
-
+    url = sys.argv[1]
+    logFile(url)
 
 
 if __name__ == "__main__":
